1ab2a161746aca25a42615cc1f6277ff0c1988ff
Davenof <david.ofili@outlook.com>
2022-10-25 00:13:05 +0100

    Create graphics.php

---
+++ graphics.php
@@ -0,0 +1,169 @@
+<?php
+    include_once 'header.php';
+?>
+
+<?php
+    include_once 'sidebar.php';
+?>
+
+            <!-- Main Content -->
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-5 mt-5 mb-5">
+                        <h3 class="page-title mt-4">PROJECTS</h3>
+
+                        <!-- PROTOTYPES -->
+                        <h3 class="lead mt-5">Graphics / Branding</h3> 
+                        <ul class="small d-inline-flex flex-wrap gap-4">
+                            <li><a href="coded.php">Hand-coded Websites</a></li>
+                            <li><a href="wordpress.php">WordPress Websites</a></li>
+                            <li><a href="webapps.php">Web Applications</a></li>
+                            <li><a href="prototypes.php">Prototypes</a></li>
+                            <li><a href="abm.php">Agent-Based Modelling</a></li>
+                        </ul>                       
+                            
+                        <!--GRAPHICS TAB -->
+                        <div class="card-header mt-5"><h5>Official Company Logos</h5></div>
+                        <div class="lead mt-2">
+                            <ul>
+                                <li>Only logos accepted by organisations/companies as official are included in the list.</li>
+                                <li>The list is not exhaustive as a few others are currently not reachable.</li>
+                            </ul>
+                        </div>
+                        <div class="row mt-5 mb-2">
+                            <!--JHL Logo --> 
+                            <div class="card col-lg-4 border-0 mb-4">
+                                <div class="training-page card-header">
+                                    <img src="img/projects/graphics/1.jpg" width="300" height="auto" alt="University of Benin Library" title="Uniben Library Logo">
+                                    <p class="card-title text-center mt-2">The Logo of the University of Benin Library, Nigeria <br>
+                                        Designed: <span class="text-muted">2014</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span><br> &nbsp;
+                                    </p>
+                                    <div class="accordion" id="accordionExample1">
+                                        <div class="accordion-item">
+                                            <div class="accordion-header" id="headingOne">
+                                                <span class="btn btn-primary col-12 text-white text-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><h5>Other Info</h5></span>
+                                            </div>
+                                            <div class="accordion-collapse collapse" id="collapseOne" aria-labelledby="headingOne">
+                                                <div class="accordion-body">
+                                                I also designed the cover pages of the first editions of the university's Journals of Science and Technology, and the Humanities, respectively.
+                                                Both journals were managed by the University Library. 
+                                                <br><br>
+                                                Other of my graphical inputs included but were not limited to event flyers, posters, banners, business cards
+                                                    for some principal officers, letterheads, among others.     
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>     
+                                </div>
+                            </div>  
+                            <!--CERHI Logo -->  
+                            <div class="card col-lg-4 border-0 mb-4">
+                                <div class="training-page card-header">
+                                    <a href="https://www.cerhiuniben.edu.ng/" target="_target"><img src="img/projects/graphics/2.jpg" width="300" height="auto" alt="CERHI Logo" title="CERHI Logo"></a>
+                                    <p class="card-title text-center mt-2">The Logo of a World Bank Project <br>
+                                        (<a href="https://www.cerhiuniben.edu.ng/" target="_target" class="small">Centre of Excellence in Reproductive Health Innovation</a>) <br>
+                                        Designed: <span class="text-muted">2014</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span>
+                                    </p>
+                                    <div class="accordion" id="accordionExample2">
+                                        <div class="accordion-item">
+                                            <div class="accordion-header" id="headingTwo">
+                                                <span class="btn btn-primary col-12 text-white text-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><h5>Other Info</h5></span>
+                                            </div>
+                                            <div class="accordion-collapse collapse" id="collapseTwo" aria-labelledby="headingTwo">
+                                                <div class="accordion-body">
+                                                Other logos were designed for the Centre by other people, including contractors, but mine was chosen by the 
+                                                Centre's Board as the official logo of the Centre because it depicted everything that defined the Centre - 
+                                                reproduction (the family), academics (the graduation cap and scroll), Nigeria (the traditional dresses), 
+                                                the University of Benin (the colours - purple, green, and gold), Africa (the map), 
+                                                an international centre (the plant, similar to that in the United Nations logo), and innovation (the concept).
+                                                <br><br> 
+                                                I also designed the letter head of the Centre, and the initial flyers used to advertise the Centre's programmes. 
+                                                
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>     
+                                </div>
+                            </div>
+                            <!--LAG Logo --> 
+                            <div class="card col-lg-4 border-0 mb-4">
+                                <div class="training-page card-header">
+                                    <img src="img/projects/graphics/3.jpg" width="300" height="auto" alt="Library Advocacy Group Logo" title="LAG Logo">
+                                    <p class="card-title text-center mt-2">Library Advocacy Group Logo <br>
+                                        Designed: <span class="text-muted">2017</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span> <br> 
+                                    </p>
+                                    <br><br>
+                                    <div class="accordion" id="accordionExample3">
+                                        <div class="accordion-item">
+                                            <div class="accordion-header" id="headingThree">
+                                                <span class="btn btn-primary col-12 text-white text-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><h5>Other Info</h5></span>
+                                            </div>
+                                            <div class="accordion-collapse collapse" id="collapseThree" aria-labelledby="headingThree">
+                                            <div class="accordion-body">
+                                                I also designed the letterhead of the group which was the foremost and most influencial advocacy group 
+                                                for libraries and librarians in Nigeria.      
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>     
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-2 mb-2">
+                            <!--DC Logo --> 
+                            <div class="card col-lg-4 border-0 mb-4">
+                                <div class="training-page card-header">
+                                    <a href="https://digitalcitizens.tech/" target="_target"><img src="img/projects/graphics/4.jpg" width="300" height="auto" alt="Digital Citizens Logo" title="DC Logo"></a>
+                                    <p class="card-title text-center mt-2">Digital Citizens Logo <br>
+                                        Designed: <span class="text-muted">2018</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span> <br> 
+                                    </p>
+                                    <br><br>
+                                    <div class="accordion" id="accordionExample4">
+                                        <div class="accordion-item">
+                                            <div class="accordion-header" id="headingFour">
+                                                <span class="btn btn-primary col-12 text-white text-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour"><h5>Other Info</h5></span>
+                                            </div>
+                                            <div class="accordion-collapse collapse" id="collapseFour" aria-labelledby="headingFour">
+                                            <div class="accordion-body">
+                                                I also designed the letterhead, business cards, ID cards, and invoice of the company, as well as several
+                                                flyers and posters for events.      
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>     
+                                </div>
+                            </div>
+                        </div> 
+                        <!--LETTERHEADS -->  
+                        <div class="card-header mt-5"><h5>Official Company Letterheads</h5></div>
+                        <div class="lead mt-2">
+                            <ul>
+                                <li>Only letterheads accepted by organisations/companies as official are included in the list.</li>
+                                <li>The list is not exhaustive as a few others are currently not reachable.</li>
+                            </ul>
+                        </div>
+                        <div class="row mt-5 mb-2">
+                            <!--LAG Letterhead -->  
+                            <div class="col-xl-6">
+                                <div class="card mb-4 border-0">
+                                    <a href="https://digitalcitizens.tech/" target="_target" class="text-center"><img src="img/projects/graphics/5.jpg" width="400" height="auto" alt="Digital Citizens Logo" title="LAG letterhead sample"></a>
+                                    <p class="card-title text-center mt-2">Library Advocacy Group Letterhead <br>
+                                        Designed: <span class="text-muted">2017</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span> <br> 
+                                    </p>
+                                </div>                   
+                            </div>
+                            <!--DC Letterhead --> 
+                            <div class="col-xl-6">
+                                <div class="card mb-4 border-0">
+                                    <a href="https://digitalcitizens.tech/" target="_target" class="text-center"><img src="img/projects/graphics/6.jpg" width="400" height="auto" alt="Digital Citizens Logo" title="DC letterhead sample"></a>
+                                    <p class="card-title text-center mt-2">Digital Citizens Letterhead <br>
+                                        Designed: <span class="text-muted">2018</span> &nbsp;|&nbsp; With: <span class="text-muted">CorelDRAW</span> <br> 
+                                    </p>
+                                </div>                   
+                            </div>
+                        </div>                                                   
+                    </div>
+                </main>
+<?php
+    include_once 'footer.php';
+?>
